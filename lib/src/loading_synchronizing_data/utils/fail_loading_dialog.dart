cedbd5a488fe8f5193f5dc68be490e0a59a654c7
eemzain <hudazain55@gmail.com>
2025-01-23 15:11:32 +0300

    .

--- lib/src/loading_synchronizing_data/utils/fail_loading_dialog.dart
+++ lib/src/loading_synchronizing_data/utils/fail_loading_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:shared_widgets/config/app_enum.dart';
 import 'package:shared_widgets/shared_widgets/app_dialog.dart';
+import 'package:yousentech_authentication/authentication/presentation/views/employees_list.dart';
 import 'package:yousentech_pos_local_db/yousentech_pos_local_db.dart';
 
 void failLoadingDialog() {
@@ -32,8 +33,6 @@ void failLoadingDialog() {
         await DBHelper.dropDBTable(isDeleteBasicData: true);
         await DBHelper.createDBTables();
         await DbHelper.restoreDatabase();
-        // change
-        // Get.offAll(() => const EmployeesListScreen());
-        //===
+        Get.offAll(() => const EmployeesListScreen());
       });
 }
