04ee1481543e26c584ab07603bbb4324cdae21d0
eemzain <hudazain55@gmail.com>
2025-02-03 16:22:08 +0300

    .

--- lib/yousentech_pos_loading_synchronizing_data.dart
+++ lib/yousentech_pos_loading_synchronizing_data.dart
@@ -3,4 +3,4 @@
 library yousentech_pos_loading_synchronizing_data;
 
 // export 'lib_module.dart';
-export 'loading_sync/src/presentation/views/data_loading_screen.dart';
+export 'loading_sync/src/utils/fail_loading_dialog.dart';
